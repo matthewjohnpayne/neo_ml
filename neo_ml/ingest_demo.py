2210ee438830b628bee441034e734a77a159e3b8
matthewjohnpayne <matthewjohnpayne@gmail.com>
2019-10-16 11:40:17 -0400

    initial commit of ingest-demo

---
+++ neo_ml/ingest_demo.py
@@ -0,0 +1,5 @@
+'''
+    Sample code to demonstrate the ingest of neo 'tracklet' data from sample data files
+    This is *not* intended to be a complete 'how-to' guide to a full analysis ...
+    ... It is just a demo of some simple methods to read the data into some numpy/pandas data structures
+'''
